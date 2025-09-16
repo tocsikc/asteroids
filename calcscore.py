9217f9f0f4b24055bfc648814625a17ff1ed469b
tocsikc <131706771+tocsikc@users.noreply.github.com>
2025-09-16 22:48:23 +0100

    Add High Scores

--- calcscore.py
+++ calcscore.py
@@ -1,12 +1,42 @@
-import pygame
 import time
+from colorama import init, Fore, Style
+
+init()
 
 def calc_score(time_started, asteroids_hit):
     time_end = int(time.time())
     secs_alive = time_end - time_started
-    time_alive = format_time(secs_alive)
     score = secs_alive + asteroids_hit
-    return score, time_alive
+
+    score_str, time_str = get_high_score(score, secs_alive)
+
+    return score_str, time_str
+
+def get_high_score(score, secs): # check if a new high score has been set
+    with open("data/highscore.txt", "r") as f:
+        lines = f.readlines()
+        data = lines[1].strip().split("-")
+
+        high_score = int(data[0])
+        best_time = int(data[1])
+
+        score_str = Fore.RESET + "Your Score: " + Fore.LIGHTGREEN_EX + f"{score}" + Fore.RESET + " High Score: "+ Fore.YELLOW + f"{high_score}"
+        time_str = Fore.RESET + "Time Lived: " + Fore.LIGHTGREEN_EX + f"{format_time(secs)}" + Fore.RESET + " Best Time: "+ Fore.YELLOW + f"{format_time(best_time)}"
+        
+        if score > high_score: # New high score
+            set_high_score(score, best_time)
+            score_str = Fore.LIGHTYELLOW_EX + "HIGH SCORE! " + Fore.LIGHTMAGENTA_EX + f"{score}" + Fore.RESET + " (Previous best: " +  f"{high_score})" 
+            high_score = score
+
+        if secs > best_time: # New best time
+            set_high_score(high_score, secs)
+            time_str = Fore.LIGHTYELLOW_EX + f"NEW BEST TIME! " + Fore.LIGHTMAGENTA_EX + f"{format_time(secs)}" + Fore.RESET + " (Previous best: " + f"{format_time(best_time)})" + Style.RESET_ALL
+
+    return score_str, time_str
+
+def set_high_score(score, secs): # write new high score data to file
+    with open("data/highscore.txt", "w") as f:
+        f.write(f"score-seconds\n{str(score)}-{str(secs)}")
 
 def format_time(seconds):
     min, sec = divmod(seconds, 60)
--- ondeath.py
+++ ondeath.py
@@ -1,8 +1,15 @@
 import sys
 from calcscore import calc_score
+from colorama import init, Fore, Style
+
+init()
 
 def on_death(time_started, asteroids_hit):
-    score, time_alive = calc_score(time_started, asteroids_hit)
-    print("Game over!")
-    print(f"Score: {score}\nSurvived {time_alive}")
+    score_str, time_str = calc_score(time_started, asteroids_hit)
+    print("")
+    print(Fore.RED + "=====================================")
+    print(Fore.LIGHTRED_EX + "             Game over!             ")
+    print(f"{score_str}\n{time_str}")
+    print(Fore.RED + "=====================================" + Style.RESET_ALL)
+    print("")
     sys.exit(0)
